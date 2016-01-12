ee9d4014d1866ffa1213ab3d613c3965138a176e
Elorri <etchemendy.elorri@gmail.com>
2016-01-12 13:47:48 +0100

    Add Status class

--- alexandria/src/main/java/it/jaschke/alexandria/Status.java
+++ alexandria/src/main/java/it/jaschke/alexandria/Status.java
@@ -1,5 +1,8 @@
 package it.jaschke.alexandria;
 
+import android.content.Context;
+import android.content.SharedPreferences;
+import android.preference.PreferenceManager;
 import android.support.annotation.IntDef;
 
 import java.lang.annotation.Retention;
@@ -46,5 +49,96 @@ public class Status {
 
     public static final int TABLE_STATUS_UNKNOWN = 0; /* we don't know if data store in the table is up to date */
     public static final int TABLE_SYNC_DONE = 1;
+
+
+
+
+
+
+    /**
+     *
+     * @param c Context used to get the SharedPreferences
+     * @return the network status integer type
+     */
+    @SuppressWarnings("ResourceType")
+    @NetworkStatus
+    public static int getNetworkStatus(Context c) {
+        SharedPreferences sp = PreferenceManager.getDefaultSharedPreferences(c);
+        return sp.getInt(c.getString(R.string.pref_network_status_key), INTERNET_OFF);
+    }
+
+
+    /**
+     *
+     * @param c Context used to get the SharedPreferences
+     * @return the google book serveur status integer type
+     */
+    @SuppressWarnings("ResourceType")
+    @GoogleBookApiStatus
+    public static int getGoogleBookpiStatus(Context c) {
+        SharedPreferences sp = PreferenceManager.getDefaultSharedPreferences(c);
+        return sp.getInt(c.getString(R.string.pref_google_book_api_status_key), SERVEUR_DOWN);
+    }
+
+    /**
+     *
+     * @param c Context used to get the SharedPreferences
+     * @return the book table status integer type
+     */
+    @SuppressWarnings("ResourceType")
+    @BookTableStatus
+    public static int getBookTableStatus(Context c) {
+        SharedPreferences sp = PreferenceManager.getDefaultSharedPreferences(c);
+        return sp.getInt(c.getString(R.string.pref_book_table_status_key), TABLE_STATUS_UNKNOWN);
+    }
+
+    /**
+     * Sets the network status into shared preference.  This function should not be called from
+     * the UI thread because it uses commit to write to the shared preferences. Nb:if call from
+     * UI thread use, apply instead.
+     *
+     * @param c      Context to get the PreferenceManager from.
+     * @param networkStatus The IntDef value to set
+     */
+    public static  void setNetworkStatus(Context c, @NetworkStatus int networkStatus) {
+        SharedPreferences sp = PreferenceManager.getDefaultSharedPreferences(c);
+        SharedPreferences.Editor spe = sp.edit();
+        spe.putInt(c.getString(R.string.pref_network_status_key), networkStatus);
+        spe.commit();
+    }
+
+
+    /**
+     * Sets the footballApi status into shared preference.  This function should not be called from
+     * the UI thread because it uses commit to write to the shared preferences. Nb:if call from
+     * UI thread use, apply instead.
+     *
+     * @param c      Context to get the PreferenceManager from.
+     * @param googleBookApiStatus The IntDef value to set
+     */
+    public static  void setGoogleBookApiStatus(Context c, @GoogleBookApiStatus int
+            googleBookApiStatus) {
+        SharedPreferences sp = PreferenceManager.getDefaultSharedPreferences(c);
+        SharedPreferences.Editor spe = sp.edit();
+        spe.putInt(c.getString(R.string.pref_google_book_api_status_key), googleBookApiStatus);
+        spe.commit();
+    }
+
+    /**
+     * Sets the scoreTable status into shared preference.  This function should not be called from
+     * the UI thread because it uses commit to write to the shared preferences. Nb:if call from
+     * UI thread use, apply instead.
+     *
+     * @param c      Context to get the PreferenceManager from.
+     * @param bookTableStatus The IntDef value to set
+     */
+    public static  void setBookTableStatus(Context c, @BookTableStatus int bookTableStatus) {
+        SharedPreferences sp = PreferenceManager.getDefaultSharedPreferences(c);
+        SharedPreferences.Editor spe = sp.edit();
+        spe.putInt(c.getString(R.string.pref_book_table_status_key), bookTableStatus);
+        spe.commit();
+    }
+
+
 
 }
--- footballscores/src/main/java/barqsoft/footballscores/Status.java
+++ footballscores/src/main/java/barqsoft/footballscores/Status.java
@@ -1,5 +1,8 @@
 package barqsoft.footballscores;
 
+import android.content.Context;
+import android.content.SharedPreferences;
+import android.preference.PreferenceManager;
 import android.support.annotation.IntDef;
 
 import java.lang.annotation.Retention;
@@ -40,10 +43,97 @@ public class Status {
     @IntDef({
             TABLE_STATUS_UNKNOWN,
             TABLE_SYNC_DONE })
-    public @interface ScoreTableStatus { 
+    public @interface ScoreTableStatus {
     }
 
     public static final int TABLE_STATUS_UNKNOWN = 0; /* we don't know if data store in the table is up to date */
     public static final int TABLE_SYNC_DONE = 1;
+
+
+
+
+    /**
+     *
+     * @param c Context used to get the SharedPreferences
+     * @return the serveur status integer type
+     */
+    @SuppressWarnings("ResourceType")
+    @NetworkStatus
+    public static int getNetworkStatus(Context c) {
+        SharedPreferences sp = PreferenceManager.getDefaultSharedPreferences(c);
+        return sp.getInt(c.getString(R.string.pref_network_status_key), INTERNET_OFF);
+    }
+
+
+    /**
+     *
+     * @param c Context used to get the SharedPreferences
+     * @return the football serveur status integer type
+     */
+    @SuppressWarnings("ResourceType")
+    @FootballApiStatus
+    public static int getFootballApiStatus(Context c) {
+        SharedPreferences sp = PreferenceManager.getDefaultSharedPreferences(c);
+        return sp.getInt(c.getString(R.string.pref_football_api_status_key), SERVEUR_DOWN);
+    }
+
+    /**
+     *
+     * @param c Context used to get the SharedPreferences
+     * @return the score table status integer type
+     */
+    @SuppressWarnings("ResourceType")
+    @ScoreTableStatus
+    public static int getScoreTableStatus(Context c) {
+        SharedPreferences sp = PreferenceManager.getDefaultSharedPreferences(c);
+        return sp.getInt(c.getString(R.string.pref_score_table_status_key), TABLE_STATUS_UNKNOWN);
+    }
+
+    /**
+     * Sets the network status into shared preference.  This function should not be called from
+     * the UI thread because it uses commit to write to the shared preferences. Nb:if call from
+     * UI thread use, apply instead.
+     *
+     * @param c      Context to get the PreferenceManager from.
+     * @param networkStatus The IntDef value to set
+     */
+    public static  void setNetworkStatus(Context c, @NetworkStatus int networkStatus) {
+        SharedPreferences sp = PreferenceManager.getDefaultSharedPreferences(c);
+        SharedPreferences.Editor spe = sp.edit();
+        spe.putInt(c.getString(R.string.pref_network_status_key), networkStatus);
+        spe.commit();
+    }
+
+
+    /**
+     * Sets the footballApi status into shared preference.  This function should not be called from
+     * the UI thread because it uses commit to write to the shared preferences. Nb:if call from
+     * UI thread use, apply instead.
+     *
+     * @param c      Context to get the PreferenceManager from.
+     * @param footballApiStatus The IntDef value to set
+     */
+    public static  void setFootballApiStatus(Context c, @FootballApiStatus int footballApiStatus) {
+        SharedPreferences sp = PreferenceManager.getDefaultSharedPreferences(c);
+        SharedPreferences.Editor spe = sp.edit();
+        spe.putInt(c.getString(R.string.pref_football_api_status_key), footballApiStatus);
+        spe.commit();
+    }
+
+    /**
+     * Sets the scoreTable status into shared preference.  This function should not be called from
+     * the UI thread because it uses commit to write to the shared preferences. Nb:if call from
+     * UI thread use, apply instead.
+     *
+     * @param c      Context to get the PreferenceManager from.
+     * @param scoreTableStatus The IntDef value to set
+     */
+    public static  void setScoreTableStatus(Context c, @ScoreTableStatus int scoreTableStatus) {
+        SharedPreferences sp = PreferenceManager.getDefaultSharedPreferences(c);
+        SharedPreferences.Editor spe = sp.edit();
+        spe.putInt(c.getString(R.string.pref_score_table_status_key), scoreTableStatus);
+        spe.commit();
+    }
+    
 
 }
